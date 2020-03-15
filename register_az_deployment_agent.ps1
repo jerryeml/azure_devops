85f7abaeef50db3afb3ca8ba06d55cafee89206a
Jerry He <jerry_he@trendmicro.com>
2020-03-16 00:17:46 +0800

    good

--- register_az_deployment_agent.ps1
+++ register_az_deployment_agent.ps1
@@ -1,37 +1,32 @@
-function change_az_agent_to_interactive_mode()
+<#
+    .Description
+    Register Pipeline agent to from azure pipeline line to azure agent pool     
+#>
+[CmdletBinding()]
+param 
+(
+    [parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [string] $AgentTagrget,
+    [string] $UserAccount,
+    [string] $UserPwd,
+    [string] $AzureToken,
+    [string] $Platform,
+    [string] $AgentPoolConfig,
+    [string] $AzureDevopsProjectUrl,
+    [string] $AzureDevopsProject,
+    [string] $AzureDevopsDeployGroup,
+    [string] $DeploymentTag = $AgentTagrget.Replace("-vm", ""),
+    [string] $GroupTag = $AgentTagrget.Split("-")[-1]
+)
+try
 {
-    <#
-        .Description
-            Register Pipeline agent to from azure pipeline line to azure agent pool     
-    #>
-    [CmdletBinding()]
-    param 
-    (
-    	[parameter(Mandatory=$true)]
-        [ValidateNotNullOrEmpty()]
-        [string] $AgentTagrget,
-        [string] $UserAccount,
-        [string] $UserPwd,
-        [string] $AzureToken,
-        [string] $Platform,
-        [string] $AgentPoolConfig,
-        [string] $AzureDevopsProjectUrl,
-        [string] $AzureDevopsProject,
-        [string] $AzureDevopsDeployGroup,
-        [string] $DeploymentTag = $AgentTagrget.Replace("-vm", ""),
-        [string] $GroupTag = $AgentTagrget.Split("-")[-1]
-    )
-    try
-    {
-        Start-Process -FilePath $AgentPoolConfig -NoNewWindow -ArgumentList "--unattended --deploymentGroup --url $AzureDevopsProjectUrl --auth pat --token $AzureToken --projectName $AzureDevopsProject --deploymentGroupName $AzureDevopsDeployGroup --agent $AgentTagrget-DG --replace --addDeploymentGroupTags --deploymentGroupTags `"$Platform, $DeploymentTag, $GroupTag`" --runAsAutoLogon --windowsLogonAccount $UserAccount --windowsLogonPassword $UserPwd --noRestart"
-        $nid = (Get-Process cmd).id
-        Wait-Process -Id $nid
-        return $true
-    }
-    catch
-    {
-        throw "[change_az_agent_to_interactive_mode][$($AgentTagrget)] Exception: $($_.Exception.GetType().FullName, $_.Exception.Message)"
-    }
+    Start-Process -FilePath $AgentPoolConfig -NoNewWindow -ArgumentList "--unattended --deploymentGroup --url $AzureDevopsProjectUrl --auth pat --token $AzureToken --projectName $AzureDevopsProject --deploymentGroupName $AzureDevopsDeployGroup --agent $AgentTagrget-DG --replace --addDeploymentGroupTags --deploymentGroupTags `"$Platform, $DeploymentTag, $GroupTag`" --runAsAutoLogon --windowsLogonAccount $UserAccount --windowsLogonPassword $UserPwd --noRestart"
+    $nid = (Get-Process cmd).id
+    Wait-Process -Id $nid
+    return $true
 }
-
-change_az_agent_to_interactive_mode
+catch
+{
+    throw "[register_az_deployment_agent][$($AgentTagrget)] Exception: $($_.Exception.GetType().FullName, $_.Exception.Message)"
+}
