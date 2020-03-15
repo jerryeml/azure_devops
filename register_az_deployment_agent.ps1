ce38a5358146ea390716d3ee0160e9987af59992
Jerry He <jerry_he@trendmicro.com>
2020-03-16 00:07:19 +0800

    fix format

--- register_az_deployment_agent.ps1
+++ register_az_deployment_agent.ps1
@@ -9,24 +9,24 @@ function change_az_agent_to_interactive_mode()
     (
     	[parameter(Mandatory=$true)]
         [ValidateNotNullOrEmpty()]
-	    [string] $AgentTagrget,
-	    [string] $UserAccount,
-	    [string] $UserPwd,
+        [string] $AgentTagrget,
+        [string] $UserAccount,
+        [string] $UserPwd,
         [string] $AzureToken,
         [string] $Platform,
-	    [string] $AgentPoolConfig,
-	    [string] $AzureDevopsProjectUrl,
-	    [string] $AzureDevopsProject,
+        [string] $AgentPoolConfig,
+        [string] $AzureDevopsProjectUrl,
+        [string] $AzureDevopsProject,
         [string] $AzureDevopsDeployGroup,
         [string] $DeploymentTag = $AgentTagrget.Replace("-vm", ""),
-	    [string] $GroupTag = $AgentTagrget.Split("-")[-1]
+        [string] $GroupTag = $AgentTagrget.Split("-")[-1]
     )
     try
     {
-	    Start-Process -FilePath $AgentPoolConfig -NoNewWindow -ArgumentList "--unattended --deploymentGroup --url $AzureDevopsProjectUrl --auth pat --token $AzureToken --projectName $AzureDevopsProject --deploymentGroupName $AzureDevopsDeployGroup --agent $AgentTagrget-DG --replace --addDeploymentGroupTags --deploymentGroupTags `"$Platform, $DeploymentTag, $GroupTag`" --runAsAutoLogon --windowsLogonAccount $UserAccount --windowsLogonPassword $UserPwd --noRestart"
-	    $nid = (Get-Process cmd).id
-	    Wait-Process -Id $nid
-	    return $true
+        Start-Process -FilePath $AgentPoolConfig -NoNewWindow -ArgumentList "--unattended --deploymentGroup --url $AzureDevopsProjectUrl --auth pat --token $AzureToken --projectName $AzureDevopsProject --deploymentGroupName $AzureDevopsDeployGroup --agent $AgentTagrget-DG --replace --addDeploymentGroupTags --deploymentGroupTags `"$Platform, $DeploymentTag, $GroupTag`" --runAsAutoLogon --windowsLogonAccount $UserAccount --windowsLogonPassword $UserPwd --noRestart"
+        $nid = (Get-Process cmd).id
+        Wait-Process -Id $nid
+        return $true
     }
     catch
     {
