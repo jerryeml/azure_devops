8b6bd4946a9dd4d40e635a0eb430f5ce55864fe1
Jerry He <jerry_he@trendmicro.com>
2020-03-15 23:16:13 +0800

    add new script

---
+++ register_az_deployment_agent.ps1
@@ -0,0 +1,37 @@
+function change_az_agent_to_interactive_mode()
+{
+	<#
+        .Description
+            Register Pipeline agent to from azure pipeline line to azure agent pool     
+	#>
+	[CmdletBinding()]
+	param 
+	(
+		[parameter(Mandatory=$true)]
+		[ValidateNotNullOrEmpty()]
+		[string] $AgentTagrget,
+		[string] $UserAccount,
+		[string] $UserPwd,
+        [string] $AzureToken,
+        [string] $Platform,
+		[string] $DeploymentTag = $AgentTagrget.Replace("-vm", ""),
+		[string] $GroupTag = $AgentTagrget.Split("-")[-1],
+		[string] $AgentPoolConfig = $g_GBParamTable.AZURE_VSTS_AGENT_CONFIG,
+		[string] $AzureDevopsProjectUrl = $g_GBParamTable.azure_devops_project_url,
+		[string] $AzureDevopsProject =$g_GBParamTable.azure_devops_project,
+		[string] $AzureDevopsDeployGroup = $g_GBParamTable.azure_devops_deployment_group
+	)
+	try
+	{
+		Start-Process -FilePath $AgentPoolConfig -NoNewWindow -ArgumentList "--unattended --deploymentGroup --url $AzureDevopsProjectUrl --auth pat --token $AzureToken --projectName $AzureDevopsProject --deploymentGroupName $AzureDevopsDeployGroup --agent $AgentTagrget-DG --replace --addDeploymentGroupTags --deploymentGroupTags `"$Platform, $DeploymentTag, $GroupTag`" --runAsAutoLogon --windowsLogonAccount $UserAccount --windowsLogonPassword $UserPwd --noRestart"
+		$nid = (Get-Process cmd).id
+		Wait-Process -Id $nid
+		return $true
+	}
+	catch
+	{
+		throw "[change_az_agent_to_interactive_mode][$($AgentTagrget)] Exception: $($_.Exception.GetType().FullName, $_.Exception.Message)"
+	}
+}
+
+change_az_agent_to_interactive_mode
