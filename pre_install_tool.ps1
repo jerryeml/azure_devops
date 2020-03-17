8bc99a64800819f52749944e9591e329acc461d8
Jerry He <jerry_he@trendmicro.com>
2020-03-17 19:23:44 +0800

    merge into pre install tool

--- pre_install_tool.ps1
+++ pre_install_tool.ps1
@@ -1,3 +1,11 @@
+[CmdletBinding()]
+param
+(
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string] $HostName,
+    [string] $Port="443"
+)
 ###################################################################################################
 #
 # PowerShell configurations
@@ -47,6 +55,143 @@ function Write-Log($content, $logFilePath="C:\installer\pre_install_tool.log")
 }
 
 
+function Handle-LastExitCode
+{
+    [CmdletBinding()]
+    param(
+    )
+
+    if ($LASTEXITCODE -ne 0)
+    {
+        Write-Log "The artifact failed to apply."
+        throw 'The artifact failed to apply.'
+    }
+}
+
+
+
+function New-Certificate
+{
+    [CmdletBinding()]
+    param(
+        [string] $HostName
+    )
+
+    # makecert ocassionally produces negative serial numbers, which golang tls/crypto < 1.6.1 cannot handle.
+	# https://github.com/golang/go/issues/8265
+    $serial = Get-Random
+    cd $workdir
+    .\makecert.exe -r -pe -n CN=$HostName -b 01/01/2012 -e 01/01/2022 -eku 1.3.6.1.5.5.7.3.1 -ss my -sr localmachine -sky exchange -sp "Microsoft RSA SChannel Cryptographic Provider" -sy 12 -# $serial 2>&1 | Out-Null
+
+    $thumbprint=(Get-ChildItem cert:\Localmachine\my | Where-Object { $_.Subject -eq "CN=" + $HostName } | Select-Object -Last 1).Thumbprint
+
+    if(-not $thumbprint)
+    {
+        Write-Log "Failed to create the test certificate."
+        throw 'Failed to create the test certificate.'
+    }
+
+    return $thumbprint
+}
+
+function Remove-WinRMListener
+{
+    [CmdletBinding()]
+    param(
+    )
+
+    try
+    {
+        $config = Winrm enumerate winrm/config/listener
+        foreach($conf in $config)
+        {
+            if($conf.Contains('HTTPS'))
+            {
+                Write-Log 'HTTPS is already configured. Deleting the exisiting configuration.'
+                winrm delete winrm/config/Listener?Address=*+Transport=HTTPS 2>&1 | Out-Null
+                break
+            }
+        }
+    }
+    catch
+    {
+        Write-Log "INFO: Exception while deleting the listener: $($_.Exception.Message)"
+    }
+}
+
+function Set-WinRMListener
+{
+    [CmdletBinding()]
+    param
+    (
+        [string] $HostName,
+	    [string] $Port
+    )
+
+    # Delete the WinRM Https listener, if it is already configured.
+    Remove-WinRMListener
+
+    # Create a test certificate.
+    $cert = (Get-ChildItem cert:\LocalMachine\My | Where-Object { $_.Subject -eq "CN=" + $HostName } | Select-Object -Last 1)
+    $thumbprint = $cert.Thumbprint
+    if(-not $thumbprint)
+    {
+        $thumbprint = New-Certificate -HostName $HostName
+        Write-Log "Create certificate and get thumbprint: $($thumbprint)"
+    }
+    elseif (-not $cert.PrivateKey)
+    {
+        # The private key is missing - could have been sysprepped. Delete the certificate.
+        Write-Log "The private key is missing - could have been sysprepped. Delete the certificate"
+        Remove-Item Cert:\LocalMachine\My\$thumbprint -Force | Out-Null
+        $thumbprint = New-Certificate -HostName $HostName
+    }
+
+    netsh http show sslcert ipport=0.0.0.0:$Port | Out-Null
+    if ($LastExitCode -eq 1)
+    {
+        Write-Log "No SSL Certificate Binding"
+        $WinrmCreate = "winrm create --% winrm/config/Listener?Address=*+Transport=HTTPS @{Port=`"$Port`";Hostname=`"$HostName`";CertificateThumbprint=`"$thumbPrint`"}"
+    }
+    else
+    {
+        Write-Log "SSL Certificate Already Binding to Port:($Port)"
+        $WinrmCreate = "winrm create --% winrm/config/Listener?Address=*+Transport=HTTPS @{Port=`"$Port`";Hostname=`"$HostName`";CertificateThumbprint=`"`"}"
+    }
+    Write-Log "winrm create command: $($WinrmCreate)"
+    invoke-expression $WinrmCreate
+    Handle-LastExitCode
+
+    winrm set winrm/config/service/auth '@{Basic="true"}'
+    Handle-LastExitCode
+}
+
+function Add-FirewallException
+{
+    [CmdletBinding()]
+    param(
+        [string] $Port
+    )
+
+    $ruleName = "Allow WinRm in" # Windows Remote Management (HTTPS-In)
+
+    # Determine if the rule already exists.
+    netsh advfirewall firewall show rule name=$ruleName | Out-Null
+    if ($LastExitCode -eq 0)
+    {
+        # Delete the existing rule.
+        Write-Log "Delete the existing rule"
+        netsh advfirewall firewall delete rule name=$ruleName dir=in protocol=TCP localport=$Port | Out-Null
+        Handle-LastExitCode
+    }
+
+    # Add a new firewall rule.
+    Write-Log "Add a new firewall rule."
+    netsh advfirewall firewall add rule name=$ruleName dir=in action=allow protocol=TCP localport=$Port | Out-Null
+    Handle-LastExitCode
+}
+
+
 function install_staf_framework 
 {
     netsh advfirewall firewall add rule name="Allow STAF" dir=in action=allow protocol=Any program="C:\staf\bin\stafproc.exe" | Out-Null
@@ -88,7 +233,7 @@ function install_chocolatey
 }
 
 
-function handel_firewarll_rules 
+function handel_firewarll_rules
 {
     netsh advfirewall firewall show rule name="Allow Python27" dir=in | Out-Null
     if ($LastExitCode -eq 1 )
@@ -115,6 +260,52 @@ try
     install_staf_framework
     download_azure_pipeline_agent
     install_chocolatey
+    handel_firewarll_rules
+
+    # Path for the workdir
+    $workdir = "c:\installer\"
+
+    # Check if work directory exists if not create it
+    if (Test-Path -Path $workdir -PathType Container)
+    { 
+        Write-Log "$workdir already exists" -ForegroundColor Red
+    }
+    else
+    {
+        New-Item -Path $workdir  -ItemType directory
+    }
+
+    # Download the makecert
+    $source = "https://raw.githubusercontent.com/jerryeml/azure_devops/master/makecert.exe"
+    $destination = "$workdir\makecert.exe"
+    
+    if (Get-Command 'Invoke-Webrequest')
+    {
+        Invoke-WebRequest $source -OutFile $destination
+    }
+    else
+    {
+        $WebClient = New-Object System.Net.WebClient
+        $webclient.DownloadFile($source, $destination)
+    }
+
+    Write-Log "The Host name: $($HostName) and Port: $($Port)"
+    Write-Log "Add firewall exception for port $($Port)."
+    Add-FirewallException -Port $Port
+
+    # Ensure that the service is running and is accepting requests.
+    winrm quickconfig -force
+
+    # The default MaxEnvelopeSizekb on Windows Server is 500 Kb which is very less. It needs to be at 8192 Kb.
+    # The small envelop size, if not changed, results in the WS-Management service responding with an error that
+    # the request size exceeded the configured MaxEnvelopeSize quota.
+    Write-Log 'Configuring MaxEnvelopeSize to 8192 kb.'
+    winrm set winrm/config '@{MaxEnvelopeSizekb = "8192"}'
+
+    Write-Log 'Configuring WinRM listener.'
+    Set-WinRMListener -HostName $HostName -Port $Port
+
+    Write-Log 'Artifact completed successfully.'
 }
 finally
 {
