6f763bf8a84078324c556beaa099034010e57409
Jerry He <jerry_he@trendmicro.com>
2021-01-14 00:31:13 +0800

    update executor

--- utils/powershell/template/executor.ps1
+++ utils/powershell/template/executor.ps1
@@ -9,15 +9,16 @@ param
 # Import Apex One Family params as $global_params
 $SplitTempPath = $PSScriptRoot -split $current_folder_name
 $gb_common_function_script = Join-Path -Path $SplitTempPath[0] -ChildPath "common\\common_util.psm1"
-Import-Module -Name $gb_common_function_script -Force
+Import-Module -Name $gb_common_function_script -Force -Verbose
 
 debug_log_title
 
 # execution function
+Write-Host $global_params.test
 
 
 # maintenance of logging
-create_gb_debug_log @{Message = "Good Build DebugLog Maintenance."; FileName = log_file_name{}; LineNumber = line_number{}}
+create_debug_log @{Message = "Good Build DebugLog Maintenance."; FileName = log_file_name{}; LineNumber = line_number{}}
 debug_log_maintenance -ZipName $SCRIPT_LOG_ZIP_NAME
 
 if ($result -eq $false)
