997b5e3aa64b18fe15060515f8d23b64291890f6
Jerry He <jerry_he@trendmicro.com>
2021-01-22 14:40:22 +0800

    update monitor script

--- utils/python/executable/monitor_vm_resource.py
+++ utils/python/executable/monitor_vm_resource.py
@@ -25,16 +25,38 @@ def setLogger():
     log.addHandler(fh)
 
 
-def monitor_vm_resource_in_lab(username, az_pat, sp_client_id, sp_pwd, tenant_id):
+def get_params(env, product):
+    if "int" in env:
+        dg_id_int = f"dg_id_int_{product}"
+        dg_id = load_global_params_config()["azure_devops"][dg_id_int]
+    elif "dev" in env:
+        dg_id_dev = f"dg_id_dev_{product}"
+        dg_id = load_global_params_config()["azure_devops"][dg_id_dev]
+    elif "stg" in env:
+        dg_id_stg = f"dg_id_stg_{product}"
+        dg_id = load_global_params_config()["azure_devops"][dg_id_stg]
+    elif "prod" in env:
+        dg_id_prod = f"dg_id_prod_{product}"
+        dg_id = load_global_params_config()["azure_devops"][dg_id_prod]
+    else:
+        raise AssertionError(f"env:{env} is not supported")
+
+    vg_id_product = f"vg_id_{product}"
+    vg_id = load_global_params_config()["azure_devops"][vg_id_product]
+    return vg_id, dg_id
+
+
+def monitor_vm_resource_in_lab(username, az_pat, sp_client_id, sp_pwd, tenant_id, env, product):
     az_api = AzureDevopsAPI(username, az_pat)
-    result = az_api._get_deployment_group_agent(load_global_params_config()["azure_devops"]["one_int_deployment_group_id"])
+    vg_id, dg_id = get_params(env, product)
+    result = az_api._get_deployment_group_agent(dg_id)
     # print(f"result: {result}")
     available_agent_count = jmespath.search("length(value[?contains(tags, 'available') == `true`].id)", result)
 
     if available_agent_count < 4:
         logging.info(f"available agent count: {available_agent_count} is less than 4, do provision")
         az_cli = AzureCLI(sp_client_id, az_pat, sp_pwd, tenant_id)
-        az_cli.update_var_in_variable_group(3, "available_agent_count", available_agent_count)
+        az_cli.update_var_in_variable_group(vg_id, f"{env}_available_agent", available_agent_count)
     else:
         logging.warning(f"available agent count: {available_agent_count}, no need provision")
 
@@ -48,10 +70,14 @@ if __name__ == "__main__":
     parser.add_argument("-sp-client-id", dest="sp_client_id", type=str, required=True)
     parser.add_argument("-sp-pwd", dest="sp_pwd", type=str, required=True)
     parser.add_argument("-tenant-id", dest="tenant_id", type=str, required=True)
+    parser.add_argument("-env", dest="env_name", type=str, required=True)
+    parser.add_argument("-product", dest="product", type=str, required=True)
     args = parser.parse_args()
 
     monitor_vm_resource_in_lab(username=args.username,
                                az_pat=args.az_pat,
                                sp_client_id=args.sp_client_id,
                                sp_pwd=args.sp_pwd,
-                               tenant_id=args.tenant_id)
+                               tenant_id=args.tenant_id,
+                               env=args.env_name,
+                               product=args.product)
