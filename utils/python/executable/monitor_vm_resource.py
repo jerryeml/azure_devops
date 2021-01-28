cd719bd92390c63b3128bd824916c0715795b197
Jerry He <jerry_he@trendmicro.com>
2021-01-28 23:57:40 +0800

    test new function

--- utils/python/executable/monitor_vm_resource.py
+++ utils/python/executable/monitor_vm_resource.py
@@ -21,7 +21,7 @@ class MonitorResourceUtil(object):
         self.env_and_product = env_and_product
         self.env = self.env_and_product.split('-')[0]
         self.product = self.env_and_product.split('-')[1]
-        self.vg_id, self.dg_id = self.get_params()
+        self.vg_id, self.dg_id, self.monitor_pipe_id = self.get_params()
         self.prefix = generate_random_prefix()
         self.az_api = AzureDevopsAPI(self.username, self.az_pat)
         self.az_cli = AzureCLI(self.sp_client_id, self.az_pat, self.sp_pwd, self.tenant_id)
@@ -42,6 +42,7 @@ class MonitorResourceUtil(object):
         log.addHandler(fh)
 
     def get_params(self):
+        monitor_pipe_id = load_global_params_config()["azure_devops"]['monitor_pipeline_id']
         if "int" in self.env:
             dg_id_int = f"dg_id_int_{self.product}"
             dg_id = load_global_params_config()["azure_devops"][dg_id_int]
@@ -59,7 +60,7 @@ class MonitorResourceUtil(object):
 
         vg_id_product = f"vg_id_{self.product}"
         vg_id = load_global_params_config()["azure_devops"][vg_id_product]
-        return vg_id, dg_id
+        return vg_id, dg_id, monitor_pipe_id
 
     def monitor_resource_in_lab(self):
         result = self.az_api._get_deployment_group_agent(self.dg_id)
@@ -72,7 +73,8 @@ class MonitorResourceUtil(object):
             logging.warning(f"available agent count: {available_agent_count}, no need provision")
             is_provision = True
 
-        self.az_cli.update_var_in_variable_group(self.vg_id, f"{self.env_and_product}-provision", is_provision)
+        # self.az_cli.update_var_in_variable_group(self.vg_id, f"{self.env_and_product}-provision", is_provision)
+        self.az_cli.update_var_in_pipelines(self.monitor_pipe_id, f"{self.env_and_product}-provision", is_provision)
 
     def update_tags_of_dg_agent(self):
         """
